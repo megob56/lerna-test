6902a0471ccc879d95620cbb270481550de6eb3f
Meaghan O'Brien <60037061+megob56@users.noreply.github.com>
2021-10-15 12:06:23 +0100

    refactor: refactored both packages into newer syntax

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -1,3 +1,3 @@
-module.exports = function getDate() {
+const getDate = () => {
     return new Date();
 }
--- packages/date-renderer/index.js
+++ packages/date-renderer/index.js
@@ -1,4 +1,4 @@
-const getDate = require('@megob56/date-logic');
+import getDate from '@megob56/date-logic'
 
 (() => {
   console.log(`Date: ${getDate()}`);
